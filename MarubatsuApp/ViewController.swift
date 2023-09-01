223e612f46d6a19b860dd1a55366c475c1954f0c
RyotaOnodera124 <onoryo.0124@gmail.com>
2023-09-02 08:42:47 +0900

    Initial Commit

---
+++ MarubatsuApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MarubatsuApp
+//
+//  Created by 小野寺良太 on 2023/09/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
